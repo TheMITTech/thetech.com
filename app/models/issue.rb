216074d50453a1aaa81e52abe00d7a109bbcc1fb
leonxlin <leonxlin@gmail.com>
2015-01-13 10:58:25 -0500

    Arrange issues in descending order

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,6 +4,8 @@ class Issue < ActiveRecord::Base
   validates :volume, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :number, presence: true, numericality: {only_integer: true, greater_than: 0}, uniqueness: {scope: :volume, message: 'should be unique within a volume. '}
 
+  default_scope { order('volume DESC, number DESC') }
+
   def name
     "Volume #{volume} Issue #{number}"
   end
