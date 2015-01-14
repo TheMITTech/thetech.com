28fc0c7c5d94c1614b201b976808acc80c1587dd
Samuel Barroso <fsamuel.bs@gmail.com>
2015-01-14 17:52:27 -0500

    Adds secret_key to config file.-ish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.secret_key = ENV['RAILS_SECRET_KEY']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
