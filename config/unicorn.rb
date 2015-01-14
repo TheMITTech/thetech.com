06cd7392822e9817273739a1b161bf53ce2c462e
Samuel Barroso <fsamuel.bs@gmail.com>
2015-01-14 18:03:52 -0500

    Unicorn config file.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,28 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
+# Set the working application directory
+# # working_directory "/path/to/your/app"
+working_directory "/var/www/prototype-rails"
+
+# # Unicorn PID file location
+# # pid "/path/to/pids/unicorn.pid"
+pid "/var/www/prototype-rails/pids/unicorn.pid"
+
+# # Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/www/prototype-rails/log/unicorn.log"
+stdout_path "/var/www/prototype-rails/log/unicorn.log"
+
+# # Unicorn socket
+# listen "/tmp/unicorn.[app name].sock"
+listen "/tmp/unicorn.prototype-rails.sock"
+
+# # Number of processes
+# # worker_processes 4
+worker_processes 2
+
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
