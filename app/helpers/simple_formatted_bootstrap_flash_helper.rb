7c6d827d074dc2d76b2317c0d9abb9f4e704e0c1
Jiahao Li <isundaylee.reg@gmail.com>
2015-01-14 21:41:25 -0500

    Fixes over-persisting flash.

--- app/helpers/simple_formatted_bootstrap_flash_helper.rb
+++ app/helpers/simple_formatted_bootstrap_flash_helper.rb
@@ -4,27 +4,38 @@ module SimpleFormattedBootstrapFlashHelper
   def simple_formatted_bootstrap_flash(options = {})
     flash_messages = []
 
+    collected_flash = {}
+
     @flash.each do |type, message|
-      flash[type] = message
+      collected_flash[type] ||= []
+      collected_flash[type] << message
     end
 
     flash.each do |type, message|
-      # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
-      next if message.blank?
+      collected_flash[type] ||= []
+      collected_flash[type] << message
+    end
 
-      type = type.to_sym
-      type = :success if type == :notice
-      type = :danger  if type == :alert
-      type = :danger  if type == :error
-      next unless ALERT_TYPES.include?(type)
+    collected_flash.each do |type, messages|
+      messages.each do |message|
+        # Skip empty messages, e.g. for devise messages set to nothing in a locale file.
+        next if message.blank?
 
-      Array(message).each do |msg|
-        text = content_tag(:div,
-                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
-                           msg.gsub("\n", "<br>").html_safe, :class => "alert fade in alert-#{type} #{options[:class]}")
-        flash_messages << text if msg
+        type = type.to_sym
+        type = :success if type == :notice
+        type = :danger  if type == :alert
+        type = :danger  if type == :error
+        next unless ALERT_TYPES.include?(type)
+
+        Array(message).each do |msg|
+          text = content_tag(:div,
+                             content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+                             msg.gsub("\n", "<br>").html_safe, :class => "alert fade in alert-#{type} #{options[:class]}")
+          flash_messages << text if msg
+        end
       end
     end
+
     flash_messages.join("\n").html_safe
   end
 end
