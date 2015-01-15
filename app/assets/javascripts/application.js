31afc241f9d46b9aced044a6bf99b9463c05c2cb
Samuel Barroso <fsamuel.bs@gmail.com>
2015-01-15 18:29:46 -0500

    Perform asset loading per controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require twitter/typeahead.min
 //= require angular
 //= require bootstrap
-//= require_tree .
+//= require bootstrap-tagsinput/bootstrap-tagsinput.min
 
 // shim for pretty file uploads, borrowed from http://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3/
 $(document).on('ready', function() {
