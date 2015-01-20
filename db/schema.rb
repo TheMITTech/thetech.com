0dc09e69de619d39346806f696ccc4b3c226cf72
Zygimantas Straznickas <zygi@mit.edu>
2015-01-20 11:55:03 -0500

    Resolve merge conflict'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116165225) do
+ActiveRecord::Schema.define(version: 20150116231958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,10 @@ ActiveRecord::Schema.define(version: 20150116165225) do
     t.integer  "volume"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "pdf_file_name"
+    t.string   "pdf_content_type"
+    t.integer  "pdf_file_size"
+    t.datetime "pdf_updated_at"
   end
 
   create_table "legacy_pages", force: true do |t|
