44ae94b1f704f18d2225a6dd519bf3b21d4afc5f
Stephen Suen <ssuen@mit.edu>
2015-01-21 18:47:56 -0500

    Application.js no longer requires tree.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,20 +12,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-
-// shim for pretty file uploads, borrowed from http://www.abeautifulsite.net/whipping-file-inputs-into-shape-with-bootstrap-3/
-$(document).on('ready', function() {
-
-	$('.btn-file :file').on('change', function() {
-		var input = $(this),
-        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-	    input.trigger('fileselect', [numFiles, label]);
-	});
-
-	$('.btn-file').on('fileselect', function(e, numFiles, label) {
-		$(this).parent().find('#filepath').text(label);
-	});
-});
 
