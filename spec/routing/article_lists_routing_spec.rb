1c5ee71c42519ce6e8972853c3d307de03fe8cd0
leonxlin <leonxlin@gmail.com>
2015-01-26 16:11:33 -0500

    Remove unnecessary tests

--- spec/routing/article_lists_routing_spec.rb
+++ spec/routing/article_lists_routing_spec.rb
@@ -4,31 +4,23 @@ RSpec.describe ArticleListsController, :type => :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/article_lists").to route_to("article_lists#index")
+      expect(:get => "/admin/article_lists").to route_to("article_lists#index")
     end
 
     it "routes to #new" do
-      expect(:get => "/article_lists/new").to route_to("article_lists#new")
+      expect(:get => "/admin/article_lists/new").to route_to("article_lists#new")
     end
 
     it "routes to #show" do
-      expect(:get => "/article_lists/1").to route_to("article_lists#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/article_lists/1/edit").to route_to("article_lists#edit", :id => "1")
+      expect(:get => "/admin/article_lists/1").to route_to("article_lists#show", :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/article_lists").to route_to("article_lists#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/article_lists/1").to route_to("article_lists#update", :id => "1")
+      expect(:post => "/admin/article_lists").to route_to("article_lists#create")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/article_lists/1").to route_to("article_lists#destroy", :id => "1")
+      expect(:delete => "/admin/article_lists/1").to route_to("article_lists#destroy", :id => "1")
     end
 
   end
