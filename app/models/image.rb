5a0127624971bc765ddadd9d77ed336387e29113
Jiahao Li <isundaylee.reg@gmail.com>
2015-01-26 12:51:53 -0500

    Caches Image#as_display_json.

--- app/models/image.rb
+++ app/models/image.rb
@@ -42,16 +42,18 @@ class Image < ActiveRecord::Base
   end
 
   def as_display_json
-    {
-      id: self.id,
-      primary_slug: self.primary_piece.try(:slug),
-      assigned_pieces: self.pieces.map(&:article).map(&:as_display_json),
-      caption: self.caption,
-      attribution: self.attribution,
-      section_name: self.primary_piece.try(:section).try(:name),
-      issue: {volume: self.primary_piece.try(:issue).try(:volume), number: self.primary_piece.try(:issue).try(:number)},
-      thumbnail_path: self.primary_picture_url(:thumbnail)
-    }
+    Rails.cache.fetch("#{cache_key}/display_json") do
+      {
+        id: self.id,
+        primary_slug: self.primary_piece.try(:slug),
+        assigned_pieces: self.pieces.map(&:article).map(&:as_display_json),
+        caption: self.caption,
+        attribution: self.attribution,
+        section_name: self.primary_piece.try(:section).try(:name),
+        issue: {volume: self.primary_piece.try(:issue).try(:volume), number: self.primary_piece.try(:issue).try(:number)},
+        thumbnail_path: self.primary_picture_url(:thumbnail)
+      }
+    end
   end
 
   private
