202d4b37ff47971baff6e20fee89f961fa64864f
nbravo <nbravo@mit.edu>
2015-01-29 12:26:45 -0500

    new controller tests for frontend_homepage and series

---
+++ spec/factories/homepage.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :homepage_draft, class: Homepage  do
+    status 0
+    layout []
+  end
+
+  factory :homepage_published, class: Homepage  do
+    status 1
+    layout []
+  end
+end
