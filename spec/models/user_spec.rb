03538da09e0acadb6b2673c477c5acba0e7742be
nbravo <nbravo@mit.edu>
2015-01-29 23:34:09 -0500

    fix user spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,10 @@ describe User, '#update_roles' do
   it 'correctly adds a user role' do
     # setup
     user = create(:user)
-    expect(user.role_values).to eq []
+    expect(user.role_values).to match_array []
 
     user.update_roles([UserRole::PUBLISHER])
-    expect(user.role_values).to eq [UserRole::PUBLISHER]
+    expect(user.role_values).to match_array [UserRole::PUBLISHER]
   end
 
   it 'correctly removes a user role' do
@@ -17,10 +17,10 @@ describe User, '#update_roles' do
     publisher_role = create(:publisher_role, user_id: user.id)
     editor_in_chief_role = create(:editor_in_chief_role, user_id: user.id)
     admin_role = create(:admin_role, user_id: user.id)
-    expect(UserRole.where(user_id: user.id)).to eq [publisher_role, editor_in_chief_role, admin_role]
+    expect(UserRole.where(user_id: user.id)).to match_array [publisher_role, editor_in_chief_role, admin_role]
 
     user.update_roles([UserRole::PUBLISHER])
-    expect(UserRole.where(user_id: user.id)).to eq [publisher_role]
+    expect(UserRole.where(user_id: user.id)).to match_array [publisher_role]
   end
 end
 
@@ -30,6 +30,6 @@ describe User, '#role_values' do
     user = create(:user)
     admin_role = create(:admin_role, user_id: user.id)
     editor_in_chief_role = create(:editor_in_chief_role, user_id: user.id)
-    expect(user.role_values).to eq [UserRole::ADMIN, UserRole::EDITOR_IN_CHIEF]
+    expect(user.role_values).to match_array([UserRole::ADMIN, UserRole::EDITOR_IN_CHIEF])
   end
 end
