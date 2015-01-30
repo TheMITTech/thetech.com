789fbe610200d0c93d3b58b07acde486e325eea9
Jiahao Li <isundaylee.reg@gmail.com>
2015-01-30 00:24:23 -0500

    More realistic author bio limit.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < AbstractModel
   validates :name, presence: true, length: {minimum: 1, maximum: 200}
   validates :email, presence: false, email: true, if: '!email.blank?'
-  validates :bio, length: {maximum: 1000}
+  validates :bio, length: {maximum: 10000}
 
   has_many :articles, through: :authorships
   has_many :authorships
