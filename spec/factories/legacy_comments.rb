7c8396c21ecfc7770e0e68031d1e5749cf24b093
leonxlin <leonxlin@gmail.com>
2015-01-31 00:08:35 -0500

    Fix width of legacy comment index

--- spec/factories/legacy_comments.rb
+++ spec/factories/legacy_comments.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :legacy_comment do
     piece_id 1
-author_email "MyText"
-author_name "MyText"
-published_at "2015-01-29 17:43:39"
-ip_address "MyString"
-content "MyText"
+    author_email "MyText"
+    author_name "MyText"
+    published_at "2015-01-29 17:43:39"
+    ip_address "MyString"
+    content "MyText"
   end
-
 end
