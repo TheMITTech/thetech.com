55d41c5a4c98b4005fd4e747fe0fb85744790611
Jiahao Li <isundaylee.reg@gmail.com>
2015-01-31 23:19:13 -0500

    Cache show piece pages.

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -3,4 +3,9 @@ class FrontendController < ApplicationController
     def allowed_in_frontend?
       true
     end
+
+    def set_cache_control_headers(age = 24.hours)
+      request.session_options[:skip] = true
+      response.headers["Cache-Control"] = "public, max-age=#{age}"
+    end
 end
