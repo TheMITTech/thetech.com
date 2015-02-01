3a3acd2aea54da6cf4265f24a593d5bce06d0aac
Jiahao Li <isundaylee.reg@gmail.com>
2015-02-01 00:04:03 -0500

    Cache homepage.

--- app/controllers/frontend_homepage_controller.rb
+++ app/controllers/frontend_homepage_controller.rb
@@ -1,6 +1,10 @@
 class FrontendHomepageController < FrontendController
+
+  before_action :set_cache_control_headers, only: [:show]
+
   def show
     @homepage = Homepage.published
+    set_cache_control_headers(15.minutes)
     render 'show_homepage', layout: 'frontend'
   end
 
