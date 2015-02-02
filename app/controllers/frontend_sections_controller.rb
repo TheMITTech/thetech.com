c02c50703d390a6724c19e578f2f11d98fcfcd4f
Jiahao Li <isundaylee.reg@gmail.com>
2015-02-01 20:45:12 -0500

    20 articles per page.

--- app/controllers/frontend_sections_controller.rb
+++ app/controllers/frontend_sections_controller.rb
@@ -1,7 +1,7 @@
 class FrontendSectionsController < FrontendController
   def show
     @section = Section.friendly.find(params[:id])
-    @pieces = @section.pieces.with_published_article.page(params[:page]).per(2)
+    @pieces = @section.pieces.with_published_article.page(params[:page]).per(20)
     @articles = @pieces.map(&:article).map(&:latest_published_version)
 
     render 'show', layout: 'frontend'
