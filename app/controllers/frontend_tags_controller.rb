6a95b49bbcc95c5baab0100f0042068741ea0a83
Jiahao Li <isundaylee.reg@gmail.com>
2015-02-02 18:09:09 -0500

    Tag page pagination.

--- app/controllers/frontend_tags_controller.rb
+++ app/controllers/frontend_tags_controller.rb
@@ -1,7 +1,8 @@
 class FrontendTagsController < FrontendController
   def show
     @tag = ActsAsTaggableOn::Tag.find_by(slug: params[:id])
-    @articles = Piece.tagged_with(@tag).select(&:article).map(&:article).select(&:published?).map(&:display_version).sort_by(&:created_at).reverse
+    @pieces = Piece.tagged_with(@tag).with_published_article.page(params[:page]).per(20)
+    @articles = @pieces.map(&:article).map(&:latest_published_version)
 
     render 'show', layout: 'frontend'
   end
