fc1b4e3d130fe61c3346e5e7323f1caf8254a05e
Jiahao Li <isundaylee.reg@gmail.com>
2015-02-01 20:59:27 -0500

    Fix test suite.

--- app/controllers/publishing_controller.rb
+++ app/controllers/publishing_controller.rb
@@ -2,7 +2,4 @@ class PublishingController < ApplicationController
   def dashboard
     @pending_versions = ArticleVersion.web_ready.order('created_at DESC').uniq { |v| v.article_id }
   end
-
-  def publish
-  end
 end
