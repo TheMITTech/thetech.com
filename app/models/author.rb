ecfd748ab55167b9d6bba1982ac6ca3db11f1832
Jiahao Li <isundaylee.reg@gmail.com>
2015-02-03 20:34:39 -0500

    Add author portraits.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,10 @@
 class Author < AbstractModel
+  include Paperclip::Glue
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
+
+  has_attached_file :portrait, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/default_author_portrait.png"
+  validates_attachment_content_type :portrait, :content_type => /\Aimage\/.*\Z/
 
   validates :name, presence: true, length: {minimum: 1, maximum: 200}
   validates :email, presence: false, email: true, if: '!email.blank?'
