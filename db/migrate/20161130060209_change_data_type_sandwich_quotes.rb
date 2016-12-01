6cc0496266b753381be279ce91bf467a10c9834a
CyanoKobalamyne <arpl@mit.edu>
2016-11-30 23:12:43 -0500

    Fix typo

--- db/migrate/20161130060209_change_data_type_sandwich_quotes.rb
+++ db/migrate/20161130060209_change_data_type_sandwich_quotes.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeSandwichQuotes < ActiveRecord::Migration
   def change
-    change_column :articles, :sandwich_quotes, :textgit
+    change_column :articles, :sandwich_quotes, :text
   end
 end
