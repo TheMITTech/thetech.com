c1a88903f32de8337b32b4b9432b9b32d793e6bc
Jiahao Li <isundaylee.reg@gmail.com>
2017-02-20 02:13:25 -0500

    [SITE-72] Who are we kidding... Removes spec/ directory.

--- spec/factories/article_list.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :article_list do
-    name "article list"
-  end
-end
--- spec/factories/author.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :author do
-    name "John Doe"
-  end
-
-  factory :author_one_name, class: Author do
-    name "Jane"
-  end
-end
