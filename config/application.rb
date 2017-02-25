7ab4f88fbaddf3c807039682f30f4dfed8e57341
Jiahao Li <isundaylee.reg@gmail.com>
2017-02-25 02:21:30 -0500

    [SITE-72] Uses Redis for Rails cache backend.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Thetech
 
     config.middleware.use Rack::Attack
 
+    config.cache_store = :redis_store, Rails.env.production? ? ENV["REDIS_URL"] : "redis://localhost:6379/0/cache"
+
     config.generators do |g|
       g.test_framework nil
     end
