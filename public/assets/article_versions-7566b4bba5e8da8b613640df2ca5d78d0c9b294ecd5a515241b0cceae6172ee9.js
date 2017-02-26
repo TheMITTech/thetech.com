996880cf9882bf1740443d78bc247005673f9036
Jiahao Li <isundaylee.reg@gmail.com>
2017-02-26 01:20:07 -0500

    [SITE-72] Assets precompilation.

---
+++ public/assets/article_versions-7566b4bba5e8da8b613640df2ca5d78d0c9b294ecd5a515241b0cceae6172ee9.js
@@ -0,0 +1,1 @@
+(function(){var e;e=function(){if($("#article_versions_show").length>0)return $(".edit_article_version select").change(function(){return $(this).parents("form").submit()})},$(e)}).call(this);
