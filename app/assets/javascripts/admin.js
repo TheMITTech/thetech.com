bbd5830e580485a601f65bcf734f4cc6ea3f5112
Jiahao Li <isundaylee.reg@gmail.com>
2017-02-26 02:08:05 -0500

    [SITE-72] Switches to 'ckeditor' gem.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.remotipart
-//= require ckeditor-jquery
+//= require ckeditor/init
 //= require twitter/bootstrap
 //= require typeahead.js.js
 //= require bootstrap
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w(ckeditor/config.js)
 
 # application.js and application do not include every js or css file, so we have to precompile
 # every asset manually:
