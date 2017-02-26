b703ee736bb16aa167e3baafad31119fd7907936
Jiahao Li <isundaylee.reg@gmail.com>
2017-02-26 02:02:11 -0500

    [SITE-72] Potential fix for ckeditor asset issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
+  config.assets.precompile += ['ckeditor/*']
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
