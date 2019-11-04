e96d618c5f5ec3da79b4d3893dbc6d39e6acf072
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2019-11-03 20:55:05 -0500

    Disable precompiling all assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,22 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(ckeditor/config.js)
-
-# application.js and application do not include every js or css file, so we have to precompile
-# every asset manually:
-
-# Script copied from here: http://guides.rubyonrails.org/asset_pipeline.html
-
-Rails.application.config.assets.precompile << Proc.new do |path|
-  if path =~ /\.(css|js)\z/
-    full_path = Rails.application.assets.resolve(path).to_s
-    app_assets_path = Rails.root.join('app', 'assets').to_s
-    if full_path.starts_with? app_assets_path
-      true
-    else
-      false
-    end
-  else
-    false
-  end
-end
