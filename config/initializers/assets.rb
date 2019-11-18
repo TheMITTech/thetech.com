f779dc3ab1bbfbce0a42dc2bdd3ef70ab266b91c
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-11-18 09:26:17 -0500

    Fixed pipelined asset precompiler

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,18 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(ckeditor/config.js)
+
+Rails.application.config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/
+    @assets ||= Rails.application.assets || Sprockets::Railtie.build_environment(Rails.application)
+    full_path = @assets.resolve(path)
+    app_assets_path = Rails.root.join('app', 'assets').to_s
+    if full_path.starts_with? app_assets_path
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
