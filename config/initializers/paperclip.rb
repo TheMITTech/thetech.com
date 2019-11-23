c1196dd7f7d65fdae20ae984e2098ce9874625d3
Tareq El Dandachi <tareqdandachi@gmail.com>
2019-11-23 10:51:12 -0500

    Added AWS Region

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,7 @@
 if ENV['S3_BUCKET'].present?
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_HOST_NAME']
+  Paperclip::Attachment.default_options[:s3_region] = ENV['AWS_REGION']
   Paperclip::Attachment.default_options[:s3_protocol] = :https
   Paperclip::Attachment.default_options[:s3_credentials] = {
     :bucket => ENV['S3_BUCKET'],
