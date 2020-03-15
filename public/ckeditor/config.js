54c35c77f51e562fa2cb634f80bc8e90999647c4
Áron Ricardo Perez-Lopez <arpl@tech.mit.edu>
2020-03-14 20:51:03 -0400

    Add Anchor button to editor

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -22,7 +22,7 @@ CKEDITOR.editorConfig = function( config ) {
       }
     ];
 
-    config.removeButtons = 'Cut,Copy,RemoveFormat,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript,Font,FontSize,CreateDiv,Styles';
+    config.removeButtons = 'Cut,Copy,RemoveFormat,Paste,Undo,Redo,Underline,Strike,Subscript,Superscript,Font,FontSize,CreateDiv,Styles';
     config.removeDialogTabs = 'link:advanced';
     config.height = '484px';
     config.format_tags = 'body;subsection_heading;correction';
@@ -47,4 +47,3 @@ CKEDITOR.editorConfig = function( config ) {
 
     config.contentsCss = "/ckeditor/contents.css";
 };
-
